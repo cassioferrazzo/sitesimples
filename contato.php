a6bc658336ff1f5fe3e69a8b7352dbc6f0562cc9
cassioferrazzo <cassioferrazzo@gmail.com>
2014-11-03 02:34:53 +0000

    formulario parcial

--- contato.php
+++ contato.php
@@ -2,41 +2,28 @@
     <form method="post">
         <fieldset id="form_contato">
             <legend>Entre em contato</legend>
-            <ol>
-                <li>
-                    <label for="nome">Nome:</label>
-                    <input type="text" name="nome" outofocus id="nome">
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" id="email">                    
-                </li>
-                <li>
-                    <label for="assunto">Assunto:</label>
-                    <input type="text" name="assunto" id="assunto">                    
-                </li>                
-                <li>
-                    <label for="mensagem">Mensagem</label><p>
-                    <textarea name="mensagem" id="mensagem" cols="45" rows="5" placeholder="Deixe a sua mensagem."></textarea>  
-                </li>
-                <li>
-                    <input type="submit" value="Enviar">
-                </li>                
-                
-            </ol> 
-        </fieldset>
-        
-    </form>
-    
-    
+            <div class="form-group">
+                <label for="nome" class="col-sm-1 control-label">Nome:</label>
+                <input type="text" name="nome" outofocus id="nome" class="input-xlarge" required>
+            </div>
+            <div class="form-group">            
+                <label for="email" class="col-sm-1 control-label">Email:</label>
+                <input type="email" name="email" id="email" class="input-xlarge" required>                    
+            </div>
+            <div class="form-group">   
+                <label for="assunto" class="col-sm-1 control-label">Assunto:</label>
+                <input type="text" name="assunto" id="assunto" class="input-xlarge" required>                    
+            </div>
+            <div class="form-group">   
+                <label for="mensagem" class="col-sm-1 control-label">Mensagem</label><p>                
+                    <textarea name="mensagem" id="mensagem" cols="45" rows="5" placeholder="Deixe a sua mensagem." required></textarea>  
+            </div>
+            <div class="form-group"> 
+                <div class="col-sm-offset-2 col-sm-5">
+                    <button type="submit" class="btn btn-default">Enviar</button>  
+                    <button type="reset" class="btn btn-default">Cancelar</button>  
+                </div>
+            </div>
+        </fieldset>        
+    </form>    
 </article>
-    
-    
-
-
-
-
-
-
-Nome
-Email
-Assunto
-Mensagem
