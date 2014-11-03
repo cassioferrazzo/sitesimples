9a749fbe8dcb07d79307c5f06c30d4c7f09613ef
Cássio Ferrazzo <cassioferrazzo@gmail.com>
2014-11-03 01:08:19 +0000

    parcial projeto site simples

---
+++ empresa.php
@@ -0,0 +1,13 @@
+<article>
+    <header>
+        <h1>Empresa</h1>
+    </header>
+    <p>
+        No Direito Empresarial, atividade empresarial, ou empresa, é uma 
+        atividade econômica exercida profissionalmente pelo empresário por meio 
+        da articulação dos fatores produtivos para a produção ou circulação de 
+        bens ou de serviços. O conceito jurídico de empresa não pode ser 
+        entendido como um sujeito de direito, uma pessoa jurídica, tampouco o 
+        local onde se desenvolve a atividade econômica.        
+    </p>   
+</article>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <title>Site Simples</title>
+    </head>
+    <body>
+        <header id="cabecalho">
+            <h1>Site Simples</h1>
+            <nav>
+                <ul>
+                    <li><a href="?arquivo=home.php">Home</a></li>
+                    <li><a href="?arquivo=empresa.php">Empresa</a></li>
+                    <li><a href="?arquivo=produtos.php">Produtos</a></li>
+                    <li><a href="?arquivo=servicos.php">Serviços</a></li>
+                    <li><a href="?arquivo=contato.php">Contato</a></li>
+                </ul>
+            </nav>               
+        </header> 
+        <?php
+        $arquivo = $_GET["arquivo"];
+        if(file_exists($arquivo)){
+            require($arquivo);  
+        }else{
+            require("home.php");    
+        }
+        ?>            
+        <p>OBS: Todas as descrições foram copiadas da WIKIPEDIA.</p>
+        <footer id="rodape">
+            <p>&copy; Todos os direitos reservados - <?php echo date("Y"); ?></p>
+        </footer>
+    </body>
+</html>
