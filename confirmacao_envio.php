af8508d0da93cc1411e05e556eb1ee6c5328853d
root <root@vagrant-centos65.vagrantup.com>
2014-11-05 16:35:52 +0000

    final projeto

---
+++ confirmacao_envio.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Confirmação de Envio</title>
+        <!-- BOOTSTRAP -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js"></script>     
+    </head>
+    <body>
+        <div class="container">             
+            <h3>Dados enviados com sucesso.</h3> 
+            <p>Abaixo seguem os dados que você enviou:</p>
+            <p>Nome: <?php echo$_POST['nome']; ?></p>
+            <p>Email: <?php echo$_POST['email']; ?></p>
+            <p>Assunto: <?php echo$_POST['assunto']; ?></p>
+            <p>Mensagem: <?php echo$_POST['mensagem']; ?></p>                       
+            <div class="form-group"> 
+                <div class="col-sm-offset-2 col-sm-5">
+                    <a href="index.php">VOLTAR</a>
+                </div>
+            </div>
+        </div>  
+    </body>
+</html>
