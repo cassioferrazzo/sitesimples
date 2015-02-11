67727383c6475b564408851cea098f829f22d66d
Cássio Ferrazzo <cassioferrazzo@gmail.com>
2015-02-10 23:25:33 -0200

    criado template

---
+++ template/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; Todos os direitos reservados - <?php echo date("Y"); ?></p>
+</footer>
---
+++ template/menu.php
@@ -0,0 +1,36 @@
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class=" navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="home">Site Simples</a>
+            </div> 
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="page-scroll">                                
+                        <a href="home">Home</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="empresa">Empresa</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="produtos">Produtos</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="servicos">Serviços</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="contato">Contato</a>
+                    </li>                
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
