9293b2e657b2c0fa5155873058d280403d20a440
Cássio Ferrazzo <cassioferrazzo@gmail.com>
2015-07-31 17:45:24 -0300

    Update autenticar.php

--- autenticar.php
+++ autenticar.php
@@ -5,23 +5,25 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 }
 require_once 'functions.php';
 $usuario = $_POST['usuario'];
-$senha = md5($_POST['senha']);
+$senha = $_POST['senha'];
 
 $conn = getConnection();
 $statement = $conn->prepare("SELECT "
-        . "usuario, "
         . "senha "
         . "FROM tb_login "
-        . "WHERE usuario = :usuario "
-        . "AND senha = :senha");
+        . "WHERE usuario = :usuario ");
 $statement->bindParam(':usuario', $usuario, PDO::PARAM_STR);
-$statement->bindParam(':senha', $senha, PDO::PARAM_STR);
 $statement->execute();
 $context = $statement->fetch();
 
 if ($context) {
+    if(password_verify($senha, $context['senha'])){
     $_SESSION['logado'] = true;
     header('Location: home');
+    }else {
+        $_SESSION['logado'] = false;
+        header('Location: entrar');
+    }
 } else {
     $_SESSION['logado'] = false;
     header('Location: entrar');
